c1d63792801b5c4d7bf45952412157392aba0e6d
ahaoboy <504595380@qq.com>
2025-09-28 23:35:44 +0800

    build

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-jpackage --name rhino --input ./  --main-jar rhino-all-1.8.0.jar   --main-class org.mozilla.javascript.tools.shell.Main   --runtime-image %JAVA_HOME%  --type app-image
+jpackage --name rhino --input ./  --main-jar rhino-all-1.8.0.jar   --main-class org.mozilla.javascript.tools.shell.Main   --runtime-image $JAVA_HOME  --type app-image

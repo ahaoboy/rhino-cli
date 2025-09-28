648f3e941c6871d9112e7e3ebee78d908c18f9b6
ahaoboy <504595380@qq.com>
2025-09-28 23:52:45 +0800

    ci: macos dir

--- build.sh
+++ build.sh
@@ -5,8 +5,17 @@ fi
 
 TARGET=$1
 
+RHINO_DIR="rhino"
+
+case "$(uname)" in
+  Darwin*)
+    RHINO_DIR="rhino.app"
+    ;;
+esac
+
 jpackage --name rhino --input ./  --main-jar rhino-all-1.8.0.jar   --main-class org.mozilla.javascript.tools.shell.Main   --runtime-image $JAVA_HOME  --type app-image
 
+ls -l $RHINO_DIR
 
-tar -czf ./rhino-${TARGET}.tar.gz -C rhino .
+tar -czf ./rhino-${TARGET}.tar.gz -C $RHINO_DIR .
 ls -l ./rhino-${TARGET}.tar.gz

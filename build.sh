5a986b77724a2e61f15d87bac4516e0d78f5cf77
ahaoboy <504595380@qq.com>
2025-09-28 23:46:35 +0800

    build

--- build.sh
+++ build.sh
@@ -7,11 +7,6 @@ TARGET=$1
 
 jpackage --name rhino --input ./  --main-jar rhino-all-1.8.0.jar   --main-class org.mozilla.javascript.tools.shell.Main   --runtime-image $JAVA_HOME  --type app-image
 
-mkdir ./dist
 
-cp -r ./rhino/bin ./dist
-
-ls -lh dist
-
-tar -czf ./rhino-${TARGET}.tar.gz -C dist .
+tar -czf ./rhino-${TARGET}.tar.gz -C rhino .
 ls -l ./rhino-${TARGET}.tar.gz

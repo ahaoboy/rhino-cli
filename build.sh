8838f6a862f83da9626b1075fb88ac4ebafaa597
ahaoboy <504595380@qq.com>
2025-09-28 23:54:36 +0800

    ci: remove runtime-image

--- build.sh
+++ build.sh
@@ -13,7 +13,8 @@ case "$(uname)" in
     ;;
 esac
 
-jpackage --name rhino --input ./  --main-jar rhino-all-1.8.0.jar   --main-class org.mozilla.javascript.tools.shell.Main   --runtime-image $JAVA_HOME  --type app-image
+# jpackage --name rhino --input ./  --main-jar rhino-all-1.8.0.jar --main-class org.mozilla.javascript.tools.shell.Main   --runtime-image $JAVA_HOME  --type app-image
+jpackage --name rhino --input ./ --main-jar rhino-all-1.8.0.jar --main-class org.mozilla.javascript.tools.shell.Main --type app-image
 
 ls -l $RHINO_DIR
 

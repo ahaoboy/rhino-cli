8f3f04d7fb052e84ceca7cc295e2556e5b0f7161
ahaoboy <504595380@qq.com>
2025-09-29 00:11:44 +0800

    ci: add win-console

--- build.sh
+++ build.sh
@@ -14,7 +14,13 @@ case "$(uname)" in
 esac
 
 # jpackage --name rhino --input ./  --main-jar rhino-all-1.8.0.jar --main-class org.mozilla.javascript.tools.shell.Main   --runtime-image $JAVA_HOME  --type app-image
-jpackage --name rhino --input ./ --main-jar rhino-all-1.8.0.jar --main-class org.mozilla.javascript.tools.shell.Main --type app-image
+PLATFORM=$(uname)
+
+if [[ "$PLATFORM" == "Darwin" ]] || [[ "$PLATFORM" == "Linux" ]]; then
+  jpackage --name rhino --input ./jar --main-jar rhino-all-1.8.0.jar --main-class org.mozilla.javascript.tools.shell.Main --type app-image
+else
+  jpackage --name rhino --input ./jar --main-jar rhino-all-1.8.0.jar --main-class org.mozilla.javascript.tools.shell.Main --type app-image --win-console
+fi
 
 ls -l $RHINO_DIR
 
